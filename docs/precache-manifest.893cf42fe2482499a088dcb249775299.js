08ea2c40bace60793de99d557573dcf4b2d65f84
anibal <eramosecua@gmail.com>
2020-07-12 00:38:28 -0500

    add docs

---
+++ docs/precache-manifest.893cf42fe2482499a088dcb249775299.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f13cf3b52a8b94d45db031a8b19fd95",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e643cc959ad35767e31",
+    "url": "/static/css/main.33cb20b8.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e643cc959ad35767e31",
+    "url": "/static/js/main.2663b6b8.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
